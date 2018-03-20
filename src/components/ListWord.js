ed7aa3aacd1976846a9e49aab53236d0516815f1
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-20 20:47:50 +0700

    Word

--- src/components/ListWord.js
+++ src/components/ListWord.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 
 export class ListWord extends Component {
+    genWordItem(word) {
+        const color = word.isMemorized ? 'green' : 'red';
+        return (
+            <div key={word._id}>
+                <h3 style={{ color }}>{word.en}</h3>
+                <p>{word.vn}</p>
+            </div>
+        )
+    }
+
     render() {
+        const words = [
+            { _id: 'abcd1', en: 'One', vn: 'Mot', isMemorized: true },
+            { _id: 'abcd2', en: 'Two', vn: 'Hai', isMemorized: false },
+            { _id: 'abcd3', en: 'Three', vn: 'Ba', isMemorized: false },
+            { _id: 'abcd4', en: 'Four', vn: 'Bon', isMemorized: true },
+        ];
         return (
             <div>
                 <h3>List word works!</h3>
+                {words.map(this.genWordItem)}
             </div>
         );
     }
