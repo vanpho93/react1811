f8246e33dc5c08b5000bc9f51d69e4312bee7bd5
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-20 21:18:42 +0700

    Box.js

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 
 export class Box extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { count: 100 };
+    }
     render() {
         return (
             <div>
-                <h3 style={{ color: 'green' }}>Value: 1</h3>
-                <button>Increase</button>
+                <h3 style={{ color: 'green' }}>Value: { this.state.count }</h3>
+                <button
+                    onClick={() => {
+                        this.setState(prevState => ({ count: prevState.count + 1 }));
+                        this.setState(prevState => ({ count: prevState.count + 1 }));
+                    }}
+                >
+                    Increase
+                </button>
+                <button onClick={() => this.setState({ count: this.state.count - 1 })}>
+                    Descrease
+                </button>
+                <button onClick={() => this.setState({ count: 1 })}>
+                    Reset
+                </button>
             </div>
         );
     }
